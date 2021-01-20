a8b3b8c55f65abbdc3e925ccb0b7bb2687bf2105
haydenedelson <59719221+haydenedelson@users.noreply.github.com>
2021-01-19 23:33:10 -0500

    Update timer.py

--- timer.py
+++ timer.py
@@ -26,11 +26,12 @@ class Timer:
             # Get input from user
             while not input_validated:
                 num_rounds = input('Enter number of rounds (enter "inf" for infinity): ')
+                # Check for "inf" input 
                 if num_rounds == "inf":
                     num_rounds = math.inf
                     input_validated = True
                 else:
-                    # Check input from user
+                    # Check for valid integer input
                     if check_input(num_rounds):
                         num_rounds = int(num_rounds)
                         input_validated = True
@@ -39,6 +40,8 @@ class Timer:
 
             return num_rounds
 
+
+
         # Get number of intervals per round
         def get_num_intervals(self):
             # Set flag
@@ -46,7 +49,8 @@ class Timer:
 
             # Get input from user
             while not input_validated:
-                num_intervals = input('Enter number of intervals per round: ')
+                num_intervals = input("Enter number of intervals per round: ")
+                # Check for valid integer input
                 if check_input(num_intervals):
                     num_intervals = int(num_intervals)
                     input_validated = True
@@ -54,25 +58,41 @@ class Timer:
                     print("Input error: number of intervals must be integer > 0")
             return num_intervals
 
+
+
         # Get interval lengths
         def get_interval_lengths(self, intervals_dict):
             i = 1
-            print("Enter interval lengths in terms of hours, minutes, and seconds\nseparated by colons (i.e. [hr:][min:][sec])")
-            while i <= self.intervals:                    
-                interval_length = input(f"Interval {i}: ")
+            print("Enter interval lengths in terms of hours, minutes, and seconds\nseparated by colons (i.e. [hr:][min:]sec)")
+            while i <= self.intervals:
+                # Set flag
+                input_validated = False
+
+                # Get input from user
+                while not input_validated:       
+                    interval_length = input(f"Interval {i}: ")
+                    # Check for valid integer input
+                    input_validated = True
+                    interval_split = interval_length.rstrip().split(':')
+                    for interval in interval_split:
+                        if not check_input(interval):
+                            print("Input error: interval length must be comprised of non-negative integers separated by colons. e.g. 00:00:00")
+                            input_validated = False
                     
                 # Convert interval length to seconds
-                length_in_seconds = convert_to_seconds(interval_length)
+                length_in_seconds = convert_to_seconds(interval_split)
                 
                 intervals_dict[f"interval_{i}"] = length_in_seconds
                 i += 1
         
-        # Call functions to set timer attributes
+        # Set timer attributes
         self.rounds = get_num_rounds(self)
         self.intervals = get_num_intervals(self)
         print()
         self.intervals_dict = {}
         get_interval_lengths(self, self.intervals_dict)
+
+        
 
     def go(self):
         """
@@ -118,13 +138,6 @@ class Timer:
 
                 j += 1
             i += 1
-
-    def print_intervals(self):
-        """
-        Print current interval lengths to facilitate editing/overwriting
-        """
-        for key, value in self.intervals_dict.items():
-            print(f"{key}: {value}")
 
 def main():
     timer = Timer()
