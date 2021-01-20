e5dc32ea778669130a433a22032040a3f57751b0
haydenedelson <59719221+haydenedelson@users.noreply.github.com>
2021-01-19 23:35:18 -0500

    Update helper.py
    Move interval length validation to user input section of timer.py to prevent unexpected behaviors

--- helper.py
+++ helper.py
@@ -1,5 +1,3 @@
-from tkinter import *
-
 def check_input(input):
     """
     Verify that the user entered a _positive_ integer input
@@ -11,18 +9,13 @@ def check_input(input):
 
     return check
 
-def convert_to_seconds(interval):
+def convert_to_seconds(interval_length_list):
     """
     Convert interval legnth, passed as hr:min:sec string, to seconds
     """
-    interval_length_list = interval.split(':')
     # Convert interval length to seconds
     length_in_seconds = 0
-    for j in range(len(interval_length_list)):
-        if check_input(interval_length_list[-1 - j]):
-            length_in_seconds += int(interval_length_list[-1 - j]) * (60 ** j)
-        else:
-            print("Input error: interval length must be comprised of non-negative integers separated by colons. e.g. 00:00:00")
-            return
+    for i in range(len(interval_length_list)):
+        length_in_seconds += int(interval_length_list[-1 - i]) * (60 ** i)
     
     return length_in_seconds
